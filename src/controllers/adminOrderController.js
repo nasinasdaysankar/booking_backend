2c2448857c6e176bae0c577d90d1884b53938209
Nasina Uday Sankar <udaysankar022002@gmail.com>
2026-01-05 15:47:49 +0530

    updated the FCM token

--- src/controllers/adminOrderController.js
+++ src/controllers/adminOrderController.js
@@ -1,8 +1,11 @@
-import { sequelize, Order, CafeteriaQr } from "../models/index.js";
 import { QueryTypes, Op } from "sequelize";
 import { emitNewOrder, emitOrderStatusToUser } from "../socket.js";
 import admin from "../firebase.js";
-
+import {
+  Order,
+  CafeteriaQr,
+  UserFcmToken
+} from "../models/index.js";
 
 
 console.log("--------------------------------------------------");
