e279fc812e987960a5d6e9632f6d5d436d041fd2
Nasina Uday Sankar <udaysankar022002@gmail.com>
2026-01-08 13:58:58 +0530

    updated delete feature

--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -1,3 +1,45 @@
+// import express from "express";
+// import {
+//   addMenuItem,
+//   addBulkMenuItems,
+//   getAllMenuItems,
+//   getBeveragesMenu,
+//   getMenuByCafeteria,
+//   updateSingleImage,
+//   updateMultipleImages,
+//   updateImageById,
+//   uploadBulkImages,
+//   updateMenuItem,
+//   getMostLovedItems
+// } from "../controllers/menuController.js";
+// import { updateCategoryBulk } from "../controllers/menuController.js";
+// import multer from "multer";
+
+
+
+// const router = express.Router();
+// const upload = multer({ dest: "uploads/" });
+
+
+// router.post("/add", addMenuItem);              // Single add
+// router.post("/add/bulk", addBulkMenuItems);    // Bulk Add (CATEGORY SUPPORTED) ✔
+// router.get("/", getAllMenuItems);
+// router.get("/category/:category", getBeveragesMenu);
+// router.get("/cafeteria/:id", getMenuByCafeteria);
+// router.put("/update-image/:id", updateSingleImage);
+// router.put("/update-images", updateMultipleImages);
+// router.put("/update-category", updateCategoryBulk);
+// router.put("/update-image-url", updateImageById);
+// router.post("/upload-bulk-images", upload.array("images"), uploadBulkImages);
+// router.put("/update/:id", updateMenuItem);
+// router.get("/most-loved", getMostLovedItems);
+
+
+
+
+// export default router;
+
+
 import express from "express";
 import {
   addMenuItem,
@@ -15,8 +57,6 @@ import {
 import { updateCategoryBulk } from "../controllers/menuController.js";
 import multer from "multer";
 
-
-
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
 
@@ -33,8 +73,7 @@ router.put("/update-image-url", updateImageById);
 router.post("/upload-bulk-images", upload.array("images"), uploadBulkImages);
 router.put("/update/:id", updateMenuItem);
 router.get("/most-loved", getMostLovedItems);
-
-
+router.delete("/delete/:id", deleteMenuItem);
 
 
 export default router;
