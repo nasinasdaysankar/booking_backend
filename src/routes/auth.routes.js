97509f3fce5101db5d130ba30bfe8fe7a11ccba1
Nasina Uday Sankar <udaysankar022002@gmail.com>
2026-01-23 20:31:30 +0530

    updated the google singin

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,24 +1,24 @@
 import express from "express";
 import {
-  // register,
-  // login,
-  // googleLogin,
-  sendOtp,
-  verifyOtp,
-  saveFcmForOtp,
+  register,
+  login,
+  googleLogin,
+  // sendOtp,
+  // verifyOtp,
+  // saveFcmForOtp,
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
-//
-// router.post("/register", register);
-// router.post("/login", login);
-// router.post("/google", googleLogin);
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/google", googleLogin);
 
 //✅ OTP ROUTES (FIXED)
-router.post("/send-otp", sendOtp);
-router.post("/verify-otp", verifyOtp);
-router.post("/save-fcm", saveFcmForOtp);
+// router.post("/send-otp", sendOtp);
+// router.post("/verify-otp", verifyOtp);
+// router.post("/save-fcm", saveFcmForOtp);
 
 
 export default router;
