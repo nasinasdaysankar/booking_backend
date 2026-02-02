3509d76da323ade319351ccaffcfc88423299052
Nasina Uday Sankar <udaysankar022002@gmail.com>
2026-02-03 00:33:08 +0530

    updated the code

--- src/config/aws_s3.js
+++ src/config/aws_s3.js
@@ -1,5 +1,10 @@
 // src/config/s3.js
 import { S3Client } from "@aws-sdk/client-s3";
+import "dotenv/config"; // ✅ FORCE UPDATE ENV
+
+console.log("🔹 AWS S3 Config Loaded");
+console.log("🔹 Region:", process.env.AWS_REGION);
+console.log("🔹 Bucket:", process.env.AWS_BUCKET_NAME);
 
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
